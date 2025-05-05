6c12bb85a66ac7970ad875bcfcf07cbfb4b2b742
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-05-04 23:24:12 -0600

    changing initial setupt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
+from pydantic import BaseModel
+from typing import Union
 
 app = FastAPI(title="AI Store Assistant", description="AI Store Assistant is a tool that helps you manage your store")
 router = APIRouter()
@@ -8,14 +10,14 @@ router = APIRouter()
 async def read_users():
     return [{"username": "Rick"}, {"username": "Morty"}]
 
+@router.get("/")
+async def read_root():
+    return {"message": "AI Store Assistant is running..."}
+
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Union[bool, None] = None
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
