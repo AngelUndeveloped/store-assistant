a1c4016e6c219210dd82d4b4bc230ecaf49b75b6
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-05-06 16:32:07 -0600

    add api router

---
+++ app/api/routes.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, UploadFile, File
+from app.services.stt_service import transcribe_audio
+
+router = APIRouter()
+
+@router.get("/")
+async def root():
+    return {"message": "Store Assistant API is running."}
+
+@router.post("/speech-to-text")
+async def speech_to_text(audio: UploadFile = File(...)):
+    try:
+        text = transcribe_audio(audio)
+        return {"text": text}
+    except Exception as e:
+        return {"error": str(e)}
--- app/services/stt_service.py
+++ app/services/stt_service.py
@@ -4,4 +4,10 @@ import tempfile
 
 model = whisper.load_model("base") # change model if needed
 
-# def transcribe_audio(file:UploadFile)
+def transcribe_audio(file:UploadFile) -> str:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as tmp_file:
+        tmp_file.write(file.file.read())
+        tmp_file_path = tmp_file.name
+    
+    result = model.transcribe(tmp_file_path)
+    return result["text"]
