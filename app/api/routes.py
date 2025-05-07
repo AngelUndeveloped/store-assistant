f1a89bde53ed2e5c187d13a9c255abeb3ddff10d
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-05-06 18:56:48 -0600

    implemented speech to text using whisper

--- app/api/routes.py
+++ app/api/routes.py
@@ -7,10 +7,14 @@ router = APIRouter()
 async def root():
     return {"message": "Store Assistant API is running."}
 
-@router.post("/speech-to-text")
+@router.post("/speech-to-text/")
 async def speech_to_text(audio: UploadFile = File(...)):
     try:
         text = transcribe_audio(audio)
         return {"text": text}
     except Exception as e:
         return {"error": str(e)}
+    
+@router.get("/speech-to-text/")
+async def speech_to_text_get():
+    return {"message": "Speech to text API is running."}
--- app/main.py
+++ app/main.py
@@ -1,30 +1,6 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
-from pydantic import BaseModel
-from typing import Union
+from app.api.routes import router as stt_router
 
 app = FastAPI(title="AI Store Assistant", description="AI Store Assistant is a tool that helps you manage your store")
-router = APIRouter()
-
-@router.get("/users/", tags=["users"])
-async def read_users():
-    return [{"username": "Rick"}, {"username": "Morty"}]
-
-@router.get("/")
-async def read_root():
-    return {"message": "AI Store Assistant is running..."}
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id:": item_id, "q:": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-app.include_router(router=router)
+app.include_router(router=stt_router)
