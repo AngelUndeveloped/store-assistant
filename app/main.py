00fc916668827347a8ff52568f2da2a80254cd93
AngelUndeveloped <angelyairmendezsandoval@gmail.com>
2025-05-20 23:10:52 -0600

    an attempt was made to link the front and back end of these projects

--- app/main.py
+++ app/main.py
@@ -7,12 +7,32 @@ The application provides an API for store management with speech-to-text capabil
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router as stt_router
 
 app = FastAPI(
     title="AI Store Assistant",
-              description="AI Store Assistant is a tool that helps you manage your store"
-              )
+    description="AI Store Assistant is a tool that helps you manage your store"
+)
+
+# Configure CORS
+origins = [
+    "http://192.168.0.220:3000",  # Your frontend URL
+    "http://localhost:3000",       # Local development
+    "http://127.0.0.1:3000",      # Alternative local URL
+    "http://localhost:8000",
+    "http://localhost:8000/nlp",
+    "http://127.0.0.1:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    # allow_origins=origins,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the speech-to-text router
 app.include_router(router=stt_router)
